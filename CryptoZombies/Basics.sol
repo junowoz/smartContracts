e8a0deb84c93d648c120958dfe78d123f16a0776
junowoz <junogouvea@gmail.com>
2022-08-14 23:36:36 -0400

    1

---
+++ CryptoZombies/Basics.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+//Capitulo 2: Contratos
+
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+    constructor() {
+        
+    }
+}
+
+//Variáveis de Esado e Inteiros
