5237ff236c39b55d4577d97010414c3f2fc1a9f8
junowoz <junogouvea@gmail.com>
2022-08-15 21:59:04 -0400

    15/08/end

--- CryptoZombies/Basics.sol
+++ CryptoZombies/Basics.sol
@@ -57,4 +57,23 @@ uint[] dinamicArray;
 //Também é possível criar arrays de estruturas
 Person[] people; //Criar um array dinâmico serve para guardar dados estruturados no contrato [base de dados]
 
-//Arrays públicos
+//Arrays públicos: Outros contracts poderão ler o array (não escrever.)
+Person[] public people;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // comece aqui
+    Zombie[] public zombies;
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//Declarações de Funções 
