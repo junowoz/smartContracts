1c1a1fef29bf6aeea636eda8ec12b25cf8a4fa7c
junowoz <junogouvea@gmail.com>
2022-11-14 12:01:14 -0400

    sol

---
+++ 1-Estrutura de um contrato.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+import "meucontrato.sol"; //Serve pra importar outros contratos
+
+/******************************************************************** */
+//Estrutura de um contrato
+contract meuContrato {
+    //Dados
+}
+
+/******************************************************************** */
+//Variáveis
+contract meuContrato {
+    uint minhaIdade;
+    string nome;
+    address carteiraOuContrato;
+    bool estaAtivo;
+
+    byter32 data; //qualquer dado até 32 bytes, muito usado com gas
+     
+    //array vs lista
+}
